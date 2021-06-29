e472fcd29423f2f76f6cbf43bdfabf1bd76e6aa0
alihasnain-cd <ali.hasnain@codedistrict.com>
2021-06-29 16:34:36 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumCardComponent } from './UI/view-models/album-card/album-card.component';
+import { CircularProgressIndicatorComponent } from './UI/common/circular-progress-indicator/circular-progress-indicator.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { AlbumGateway } from './domain/models/Album/gateway/album-gateway';
+import { AlbumApiService } from './infraestructure/driven-adapter/album-api/album-api.service';
+import { AlbumApiServiceWithoutDelay } from './infraestructure/driven-adapter/album-api/album-api-withou-delay.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumCardComponent,
+    EmptyRouteComponent,
+    CircularProgressIndicatorComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [{ provide: AlbumGateway, useClass: AlbumApiServiceWithoutDelay }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
